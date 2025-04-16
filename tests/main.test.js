1f024ee46ee44a5f13954a319c0aef488158cfc2
Aurora2025-09 <hajnalka.fabian@avgi.se>
2025-04-16 21:11:15 +0200

    Update main.test.js

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,16 +1,32 @@
-import * as auth from "../src/auth";
-import * as user from "../src/user";
+const { login, createUser, changePassword, users } = require("../src/auth");
 
-describe("Main logic", () => {
-  test("Bejelentkezés meghívása működik", () => {
-    const users = [{ username: "test", password: "Test1234" }];
-    const result = auth.login("test", "Test1234", users);
+describe("Main menu funkcionalitás (közvetve)", () => {
+  beforeEach(() => {
+    users.length = 0;
+    users.push({ username: "teszt", password: "Teszt1234" });
+  });
+
+  test("Sikeres bejelentkezés", () => {
+    const result = login("teszt", "Teszt1234");
+    expect(result).toBe(true);
+  });
+  test("Sikertelen bejelentkezés hibás jelszóval", () => {
+    const result = login("teszt", "rossz", users);
+    expect(result).toMatch(/hiba/i);
+  });
+
+  test("Új felhasználó létrehozása helyes adatokkal", () => {
+    const result = createUser("ujuser", "UjJelszo1", users);
+    expect(result).toEqual({ username: "ujuser", password: "UjJelszo1" });
+  });
+
+  test("Jelszó módosítása helyes adatokkal", () => {
+    const result = changePassword("teszt", "Teszt1234", "UjJelszo1", users);
     expect(result).toBe(true);
   });
 
-  test("Új felhasználó létrehozása hibás jelszóval", () => {
-    const users = [];
-    const result = user.createUser("newuser", "short", users);
+  test("Jelszó módosítás hibás régi jelszóval", () => {
+    const result = changePassword("teszt", "rossz", "UjJelszo1", users);
     expect(result).toMatch(/hiba/i);
   });
 });
